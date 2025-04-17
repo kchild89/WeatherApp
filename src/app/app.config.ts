dc271a999c44a4614a5b50cac375318b67fb1ad1
Kevin Child <kchild89@gmail.com>
2025-04-17 09:59:23 -0600

    update on app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AppComponent } from './app/app.component';
-import { environment } from './enviroments/enviroment';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [

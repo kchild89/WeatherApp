7d56ca2814ac43e6cf15cf7ae5761a8f636f9d17
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2025-04-29 08:10:37 -0600

    testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiKey: 'AIzaSyB0IU_X7blShIsNY6UT_Z1FdIrReaYX41c',
     authDomain: 'weatherapp-2098c.firebaseapp.com',
     projectId: 'weatherapp-2098c',
-    storageBucket: 'weatherapp-2098c.appspot.com', // fix: `.app` → `.appspot.com`
+    storageBucket: 'weatherapp-2098c.appspot.com',
     messagingSenderId: '450331467319',
     appId: '1:450331467319:web:5367588421795cb3bca9b2',
   },

876907b41ffceb0b6a483a67c09ea6308b112e1d
Kevin Child <kchild89@gmail.com>
2025-04-29 10:00:02 -0600

    update profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
-  userName = 'Tommy';
-  email = 'tommy@example.com';
+export class ProfileComponent implements OnInit {
+  user: User | null = null;
+
+  ngOnInit(): void {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+        console.log('User loaded:', user);
+      } else {
+        this.user = null;
+      }
+    });
+  }
+
+  logout(): void {
+    getAuth()
+      .signOut()
+      .then(() => {
+        window.location.href = '/login';
+      });
+  }
 }

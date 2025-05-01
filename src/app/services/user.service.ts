5dfa8b733559bac0efb20bc7f0fc4ac90f75e74e
runaround <runaroundontheground@gmail.com>
2025-05-01 09:21:53 -0600

    added get and update user settings functions

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,65 @@
+import {
+  EnvironmentInjector,
+  inject,
+  Injectable,
+  runInInjectionContext,
+} from '@angular/core';
+import {
+  collection,
+  CollectionReference,
+  doc,
+  Firestore,
+  getDoc,
+  setDoc,
+} from '@angular/fire/firestore';
+import { UserSettings } from '../interfaces/user-settings';
+import { Auth } from '@angular/fire/auth';
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private auth = inject(Auth);
+  private firestore = inject(Firestore);
+  private injector = inject(EnvironmentInjector);
+
+  constructor() {}
+
+  getUserSettings() {
+    return runInInjectionContext(this.injector, () => {
+      const user = this.auth.currentUser;
+      if (user) {
+        try {
+          const settingsRef = doc(
+            this.firestore,
+            `users/${user.uid}/settings/data`
+          );
+          return from(getDoc(settingsRef));
+        } catch (error) {
+          console.error('error while getting user settings: ', error);
+        }
+      }
+      return from([null]);
+    });
+  }
+
+  updateUserSettings(settings: UserSettings) {
+    return runInInjectionContext(this.injector, () => {
+      const user = this.auth.currentUser;
+      if (user && settings) {
+        try {
+          const settingsRef = doc(
+            this.firestore,
+            `users/${user.uid}/settings/data`
+          );
+          setDoc(settingsRef, settings);
+        } catch (error) {
+          console.error('error updating user settings: ', error);
+        }
+      } else {
+        console.error('invalid user');
+      }
+    });
+  }
+}

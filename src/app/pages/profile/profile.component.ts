d11ef06bb6bc874119b0f5216e83c23667674eb9
runaround <runaroundontheground@gmail.com>
2025-05-01 09:23:03 -0600

    add units settings, updates based on user's saved settings

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { UserService } from '../../services/user.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { UserSettings } from '../../interfaces/user-settings';
+import { Observable } from 'rxjs';
+import { DocumentData, DocumentSnapshot } from 'firebase/firestore';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  private userService = inject(UserService);
   user: User | null = null;
+  userSettings$:
+    | Observable<DocumentSnapshot<DocumentData, DocumentData> | null>
+    | undefined;
+  form = new FormGroup({
+    units: new FormControl('imperial'),
+  });
 
   ngOnInit(): void {
     const auth = getAuth();
@@ -20,6 +41,19 @@ export class ProfileComponent implements OnInit {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         this.user = user;
+        this.userSettings$ = this.userService.getUserSettings();
+        this.userSettings$.subscribe((settings) => {
+          let data = settings?.data();
+          if (data === undefined) {
+            // user's settings don't exist, create default settings
+            const defaultSettings = {
+              units: 'metric',
+            };
+            data = defaultSettings;
+            this.userService.updateUserSettings(defaultSettings);
+          }
+          this.form.get('units')?.setValue(data['units']);
+        });
         console.log('User loaded:', user);
       } else {
         this.user = null;
@@ -33,5 +67,21 @@ export class ProfileComponent implements OnInit {
       .then(() => {
         window.location.href = '/login';
       });
+  }
+
+  updateSettings() {
+    const updatedSettings = {
+      units: this.form.get('units')?.value,
+    };
+    if (
+      updatedSettings &&
+      updatedSettings.units &&
+      (updatedSettings.units === 'metric' ||
+        updatedSettings.units === 'imperial')
+    ) {
+      this.userService.updateUserSettings(updatedSettings as UserSettings);
+    } else {
+      console.error('invalid input');
+    }
   }
 }

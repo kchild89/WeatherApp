9cbab0152ebc08dac192b90eb606bc4c58cd091a
Kevin <kchild89@gmail.com>
2025-05-09 08:06:43 -0600

    deleted dist from gitignore

---
+++ dist/weather-app/browser/chunk-TPGYCWBQ.js
@@ -0,0 +1,1 @@
+var e={production:!1,firebase:{apiKey:"AIzaSyB0IU_X7blShIsNY6UT_Z1FdIrReaYX41c",authDomain:"weatherapp-2098c.firebaseapp.com",projectId:"weatherapp-2098c",storageBucket:"weatherapp-2098c.appspot.com",messagingSenderId:"450331467319",appId:"1:450331467319:web:5367588421795cb3bca9b2"},weatherApiKey:"d93585013b7a4dd1b6c145210252904"};export{e as a};
